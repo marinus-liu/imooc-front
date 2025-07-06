67545955be2375b33a488c3cc9bd51db75c1cbd9
marinus-liu <marinusliu@163.com>
2025-07-06 17:24:28 +0800

    添加点击图形验证码就触发更新事件

--- src/App.vue
+++ src/App.vue
@@ -10,18 +10,19 @@
       </div>
       <div class="layui-form-item">
         <label class="layui-form-label">密码</label>
-        <div class="layui-input-inline">
+        <div class="layui-input-block">
           <input type="password" name="密码" required lay-verify="required" placeholder="请输入密码" autocomplete="off"
             class="layui-input">
         </div>
-        <div class="layui-form-mid " v-html="svg"></div>
+        
       </div>
       <div class="layui-form-item">
         <label class="layui-form-label">验证码</label>
-        <div class="layui-input-block">
+        <div class="layui-input-inline">
           <input type="text" name="title" required lay-verify="required" placeholder="请输入验证码" autocomplete="off"
             class="layui-input">
         </div>
+        <div class="layui-form-mid svg" v-html="svg" @click="getCaptcha()"></div>
       </div>
       <button class="layui-btn">登录</button>
       <a href="http://www.layui.com" class="imooc-link">找回密码</a>
@@ -39,7 +40,11 @@ export default {
     }
   },
   mounted() {
-    axios.get('http://localhost:3000/demo').then((res) => {
+    this.getCaptcha()
+  },
+  methods: {
+    getCaptcha () {
+      axios.get('http://localhost:3000/getCaptcha').then((res) => {
       if ( res.status === 200){
         let obj =  res.data
         if (obj.code === 200) {
@@ -48,6 +53,7 @@ export default {
       }
       console.log(res)
     })
+    }
   }
 }
 </script>
@@ -70,5 +76,9 @@ input {
   &:hover {
     color: #009688;
   }
+}
+.svg {
+  position: relative;
+  top: -15px;
 }
 </style>
