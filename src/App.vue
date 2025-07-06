8a85e8c5cefd015557bae0de3764d0f4d3ea86f5
marinus-liu <marinusliu@163.com>
2025-07-06 16:25:13 +0800

    1.vue create imooc-front , 不选lint ,eslint 检查代码是否合法，在编译过程中总是出现依赖包版本冲突，目前的水平无法解决故而不选 2.编辑登录页面，实现跨域请求验证码图片

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,74 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="layui-container">
+    <form class="layui-form layui-form-pane" action="">
+      <div class="layui-form-item">
+        <label class="layui-form-label">用户名</label>
+        <div class="layui-input-block">
+          <input type="text" name="用户名" required lay-verify="required" placeholder="请输入标题" autocomplete="off"
+            class="layui-input">
+        </div>
+      </div>
+      <div class="layui-form-item">
+        <label class="layui-form-label">密码</label>
+        <div class="layui-input-inline">
+          <input type="password" name="密码" required lay-verify="required" placeholder="请输入密码" autocomplete="off"
+            class="layui-input">
+        </div>
+        <div class="layui-form-mid " v-html="svg"></div>
+      </div>
+      <div class="layui-form-item">
+        <label class="layui-form-label">验证码</label>
+        <div class="layui-input-block">
+          <input type="text" name="title" required lay-verify="required" placeholder="请输入验证码" autocomplete="off"
+            class="layui-input">
+        </div>
+      </div>
+      <button class="layui-btn">登录</button>
+      <a href="http://www.layui.com" class="imooc-link">找回密码</a>
+    </form>
+  </div>
 </template>
 
-<style lang="scss">
+<script>
+import axios from "axios"
+export default {
+  name: 'app',
+  data () {
+    return {
+      svg: ''
+    }
+  },
+  mounted() {
+    axios.get('http://localhost:3000/demo').then((res) => {
+      if ( res.status === 200){
+        let obj =  res.data
+        if (obj.code === 200) {
+          this.svg =  obj.data
+        }
+      }
+      console.log(res)
+    })
+  }
+}
+</script>
+<style lang="scss" scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  background: #f2f2f2;
 }
 
-nav {
-  padding: 30px;
+.layui-container {
+  background: #fff;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+input {
+  width: 190px;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+.imooc-link {
+  margin-left: 10px;
+
+  &:hover {
+    color: #009688;
   }
 }
 </style>
