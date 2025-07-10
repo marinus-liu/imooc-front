c9f13957638a0947ac21db56276ec0ca5d6c1d46
marinus-liu <marinusliu@163.com>
2025-07-10 22:54:29 +0800

    使用vee-validate 验证表单-用户名

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="layui-container">
-    <form class="layui-form layui-form-pane" action="">
+    <Form class="layui-form layui-form-pane" @submit="checkForm">
       <div class="layui-form-item">
         <label class="layui-form-label">用户名</label>
-        <div class="layui-input-block">
-          <input type="text" name="用户名" required lay-verify="required" placeholder="请输入标题" autocomplete="off"
-            class="layui-input">
-        </div>
+        <div class="layui-input-inline">
+          <Field type="email" name="name" placeholder="请输入标题" autocomplete="off"
+            class="layui-input"  :rules="validateEmail"/>
+        </div>   
+   <div class="error layui-form-mid"> 
+    <ErrorMessage name="name"/>
+   </div>
       </div>
       <div class="layui-form-item">
         <label class="layui-form-label">密码</label>
         <div class="layui-input-block">
-          <input type="password" name="密码" required lay-verify="required" placeholder="请输入密码" autocomplete="off"
+          <input type="password" name="password" placeholder="请输入密码" autocomplete="off"
             class="layui-input">
         </div>
         
@@ -19,12 +22,12 @@
       <div class="layui-form-item">
         <label class="layui-form-label">验证码</label>
         <div class="layui-input-inline">
-          <input type="text" name="title" required lay-verify="required" placeholder="请输入验证码" autocomplete="off"
+          <input type="text" name="code" placeholder="请输入验证码" autocomplete="off"
             class="layui-input">
         </div>
         <div class="layui-form-mid svg" v-html="svg" @click="getCaptcha()"></div>
       </div>
-      <button class="layui-btn">登录</button>
+      <button class="layui-btn" >登录</button>
       <a href="http://www.layui.com" class="imooc-link">找回密码</a>
     </form>
   </div>
@@ -32,11 +35,22 @@
 
 <script>
 import axios from "axios"
+import {Field , Form, ErrorMessage} from 'vee-validate'
+
 export default {
   name: 'app',
+   components: {
+    Form,
+    Field,
+    ErrorMessage
+  },
   data () {
     return {
-      svg: ''
+      svg: '',
+      name : '',
+      code: '',
+      password: '',
+      errorMsg: []
     }
   },
   mounted() {
@@ -53,7 +67,34 @@ export default {
       }
       console.log(res)
     })
-    }
+    },
+    checkForm (values) {
+       console.log(JSON.stringify(values, null, 2));
+      this.errorMsg = []
+      if(!this.name){
+        this.errorMsg.push('用户名不能为空')
+      }
+      if(!this.code){
+        this.errorMsg.push('验证码不能为空')
+      }
+      if(!this.password){
+        this.errorMsg.push('密码不能为空')
+      }
+      console.log(this.errorMsg)
+    },
+    validateEmail(value) {
+      // if the field is empty
+      if (!value) {
+        return 'This field is required';
+      }
+      // if the field is not a valid email
+      const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+      if (!regex.test(value)) {
+        return 'This field must be a valid email';
+      }
+      // All is good
+      return true;
+    },
   }
 }
 </script>
@@ -80,5 +121,8 @@ input {
 .svg {
   position: relative;
   top: -15px;
+}
+.error{
+  color: red;
 }
 </style>
