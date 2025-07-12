ee44a981787826b46ce70faf635def2bfa827f5e
marinus-liu <marinusliu@163.com>
2025-07-12 10:48:15 +0800

    表单验证 错误提示本地化

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,42 @@
 <template>
   <div class="layui-container">
-    <Form class="layui-form layui-form-pane" @submit="checkForm">
+    <Form class="layui-form layui-form-pane" v-slot="{ errors }" @submit="checkForm">
       <div class="layui-form-item">
         <label class="layui-form-label">用户名</label>
         <div class="layui-input-inline">
           <Field type="email" name="name" placeholder="请输入标题" autocomplete="off"
-            class="layui-input"  :rules="validateEmail"/>
+            class="layui-input"  rules="required|email"/>
         </div>   
    <div class="error layui-form-mid"> 
-    <ErrorMessage name="name"/>
+   
+   {{ errors.name }}
+   
    </div>
       </div>
       <div class="layui-form-item">
         <label class="layui-form-label">密码</label>
-        <div class="layui-input-block">
-          <input type="password" name="password" placeholder="请输入密码" autocomplete="off"
-            class="layui-input">
+        <div class="layui-input-inline">
+          <Field type="password" name="password" placeholder="请输入密码" autocomplete="off"
+            class="layui-input" rules="required"/>
         </div>
+          <div class="error layui-form-mid"> 
+    {{ errors.password }}
+   
+   </div>
         
       </div>
       <div class="layui-form-item">
         <label class="layui-form-label">验证码</label>
         <div class="layui-input-inline">
-          <input type="text" name="code" placeholder="请输入验证码" autocomplete="off"
-            class="layui-input">
+          <Field type="text" name="code" placeholder="请输入验证码" autocomplete="off"
+            class="layui-input" rules="required"/>
         </div>
+          
         <div class="layui-form-mid svg" v-html="svg" @click="getCaptcha()"></div>
+        <div class="error "> 
+    {{ errors.code }}
+    
+   </div>
       </div>
       <button class="layui-btn" >登录</button>
       <a href="http://www.layui.com" class="imooc-link">找回密码</a>
@@ -35,14 +46,29 @@
 
 <script>
 import axios from "axios"
-import {Field , Form, ErrorMessage} from 'vee-validate'
+import {Field , Form, defineRule, configure} from 'vee-validate'
+import { required, email } from '@vee-validate/rules';
+import { localize } from '@vee-validate/i18n';
+import zh from '@vee-validate/i18n/dist/locale/zh_CN.json';
+
+defineRule('required', required);
+defineRule('email', email);
+localize({ zh });
+configure({
+  generateMessage: localize('zh', {
+      names:{
+        name:'用户名',
+        password:'密码',
+        code:'验证码'
+      }
+  }),
+});
 
 export default {
   name: 'app',
    components: {
     Form,
-    Field,
-    ErrorMessage
+    Field
   },
   data () {
     return {
@@ -69,8 +95,9 @@ export default {
     })
     },
     checkForm (values) {
+      console.log(values)
        console.log(JSON.stringify(values, null, 2));
-      this.errorMsg = []
+     /*  this.errorMsg = []
       if(!this.name){
         this.errorMsg.push('用户名不能为空')
       }
@@ -80,7 +107,7 @@ export default {
       if(!this.password){
         this.errorMsg.push('密码不能为空')
       }
-      console.log(this.errorMsg)
+ */      console.log(this.errorMsg)
     },
     validateEmail(value) {
       // if the field is empty
