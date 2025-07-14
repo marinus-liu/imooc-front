d79c0bfcbe656546f851f0ca2df204b86c198c80
marinus-liu <marinusliu@163.com>
2025-07-14 22:43:37 +0800

    添加页面头、属组件

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="footer">
+<p>
+    <a href="http://imooc.com" target="_blank" >Imooc 社区 2017 &copy;</a>
+    <a href="http://imooc.com" target="_blank" > layui.com</a>
+</p>
+<p>
+    <a href="http://imooc.com" target="_blank" >Imooc 社区</a>
+    <a href="http://imooc.com" target="_blank" > layui.com</a>
+</p>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+<style lang="scss" scoped>
+#footer{
+    display: flex;
+justify-content: center;
+align-items: center;
+height: 100px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,96 @@
+<template>
+    <div>
+        <div class="  clearfix" id="nav" style="background-color: #ccc !important;">
+            <div class="layui-container">
+<div class="layui-row new_header clearfix">
+                <div class="layui-col-md9">
+                    <div id="logo" class="logo"><a href="/" target="_self" title="首页"><img title="慕课网"
+                                src="../assets/logo.png"></a></div>
+                    <div class="nav-items">
+                        <ul class="nav-item">
+                            <li>
+                                标题1
+                            </li>
+                            <li>
+                                标题2
+                            </li>
+                            <li>
+                                标题3
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="layui-col-md3">
+                    <div class="header-right">
+                        <div id="login-area">
+                            <ul class="header-unlogin clearfix newcomer-box">
+                                <li class="header-signin">
+                                    <a href="//www.imooc.com/user/newlogin" id="js-signin-btn">登录</a> / <a
+                                        href="//www.imooc.com/user/newsignup" id="js-signup-btn">注册</a>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+            </div>
+            
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+<style lang="scss" scoped>
+.new_header {
+    // width: 1152px !important;
+    height: 72px;
+    // margin: 0 auto;
+    // font-size: 14px;
+}
+#logo a img{
+    width:30px;
+   margin-top: 20px;
+    float: left;
+}
+.nav-items{
+    height: 45px;
+align-content: center;
+}
+ul {
+    // list-style: none;
+    // display: block;
+    // list-style-type: disc;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    padding-inline-start: 40px;
+}
+
+ul li {
+    float: left;
+    position: relative;
+    margin-left: 15px;
+}
+
+.header_right {
+    float: right;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    height: 72px;
+}
+
+#login-area {
+    float: left;
+    position: relative;
+    margin-top: 10px;
+}
+</style>
