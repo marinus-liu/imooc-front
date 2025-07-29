9d5263a286bcc6050c4e148c04e923dddda83ebd
marinus-liu <marinusliu@163.com>
2025-07-29 22:46:23 +0800

    修正加载时总提示Feature flag __VUE_PROD_HYDRATION_MISMATCH_DETAILS__ is not explicitly define警告

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,23 @@
 const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
+/* module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     proxy: 'http://localhost:3000'
   }
-});
+}); */
+module.exports = {
+  transpileDependencies: true,
+  devServer: {
+    proxy: 'http://localhost:3000'
+  },
+chainWebpack: (config) => {
+config.plugin('define').tap((definitions) => {
+Object.assign(definitions[0], {
+__VUE_OPTIONS_API__: 'true',
+__VUE_PROD_DEVTOOLS__: 'false',
+__VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'
+})
+return definitions
+})
+}
+}
